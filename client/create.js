e04d9250d369d592b8c077c1ca7be9e785efff38
Kelvin Ishigo <kkishigo@directv.com>
2015-06-05 18:50:31 -0700

    Add support for units, conversion, stickiness

--- client/create.js
+++ client/create.js
@@ -27,6 +27,10 @@
 // convert back out to the current setting.  Valid settings: English, Metric
 Session.setDefault('userUnitsOfMeasure', 'English');
 
+var lawnData = {width: 0, length: 0, slope: 0};
+var lawnDataEnglish = {widthFeet: 0, widthInches: 0, lengthFeet: 0, lengthInches: 0, slopeInches: 0};
+Session.setDefault('computedArea', 0);
+
 Template.create.onCreated(function(){
 	console.log('Template.create.onCreated');
 });
@@ -37,6 +41,18 @@ Template.create.helpers({
 	},
 	unitsOfMeasureAre: function (unitString) {
 		return Session.get('userUnitsOfMeasure') === unitString;
+	},
+	lawnDataEnglish: function () {
+		return lawnDataEnglish;
+	},
+	lawnData: function () {
+		return lawnData;
+	},
+	computedArea: function () {
+		var sqMeters = Session.get('computedArea');
+		var sqInches = Utils.convertMetersToEnglish(sqMeters);
+		var sqFeet = sqInches / 12.0;
+		return (Session.get('userUnitsOfMeasure') === 'English') ? sqFeet + ' sq ft' : sqMeters + ' sq m';
 	}
 });
 
@@ -47,5 +63,28 @@ Template.create.events({
 	},
 	'click #lawn-measure': function (e) {
 		console.log('lawn-measure clicked');
+		if (Session.get('userUnitsOfMeasure') === 'English') {
+			lawnDataEnglish.widthFeet = document.getElementById('lawn_width_feet').value;
+			lawnDataEnglish.widthInches = document.getElementById('lawn_width_inches').value;
+			lawnDataEnglish.lengthFeet = document.getElementById('lawn_length_feet').value;
+			lawnDataEnglish.lengthInches = document.getElementById('lawn_length_inches').value;
+			lawnDataEnglish.slopeInches = document.getElementById('lawn_slope_inches').value;
+			lawnData.width = Utils.convertEnglishToMeters(lawnDataEnglish.widthFeet, lawnDataEnglish.widthInches);
+			lawnData.length = Utils.convertEnglishToMeters(lawnDataEnglish.lengthFeet, lawnDataEnglish.lengthInches);
+			lawnData.slope = Utils.convertEnglishToMeters(0, lawnDataEnglish.slopeInches);
+		}
+		else {
+			lawnData.width = document.getElementById('lawn_width_meters').value;
+			lawnData.length = document.getElementById('lawn_length_meters').value;
+			lawnData.slope = document.getElementById('lawn_slope_meters').value;
+			
+			lawnDataEnglish.widthFeet = 0;
+			lawnDataEnglish.widthInches = Utils.convertMetersToEnglish(lawnData.width);
+			lawnDataEnglish.lengthFeet = 0;
+			lawnDataEnglish.lengthInches = Utils.convertMetersToEnglish(lawnData.length);
+			lawnDataEnglish.slopeInches = Utils.convertMetersToEnglish(lawnData.slope);
+		}
+		Session.set('computedArea', Number(lawnData.width) * Number(lawnData.length));
+		console.log('lawnData: ' + lawnData);
 	}
 });
