b41cdbc2af86adcb1e2078cdda6e3e50a3da8193
kishigo <kkishigo@directv.com>
2015-08-15 23:45:02 -0700

    Support responsive carousel plus stretch to fit enclosing div

--- client/slickcarousel.js
+++ client/slickcarousel.js
@@ -31,7 +31,33 @@ var setupSlick = function setupSlick () {
 		//slidesToShow: 3,
 		//adaptiveHeight: true,
 		slidesToScroll: 3,
-		variableWidth: true
+		slidesToShow: 3,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 };
 
