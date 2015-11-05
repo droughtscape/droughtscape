d8d2e97d984bd24ff96d12be92fcfc6929482741
kishigo <kkishigo@directv.com>
2015-11-05 08:44:14 -0800

    Playing around with some meteor animation stuff.  Not working

--- client/splash.js
+++ client/splash.js
@@ -32,18 +32,24 @@ Template.splash.onCreated(function () {
 	let splashLawns = SplashManager.getSplashLawns();
 	if (splashLawns) {
 		let splashIdx = 0;
+		let testSlick = false;
 		Meteor.defer(function () {
-			//MBus.publish(Constants.mbus_carousel_add, new Message.Add(carouselIdElt, '100%', '100%', splashLawns));
-			timer = setInterval(function () {
-				if (splashLawns.length > 1) {
-					Session.set('splashItem', splashLawns[splashIdx].url);
-					Session.set('splashId', splashLawns[splashIdx].id);
-					splashIdx++;
-					if (splashIdx == splashLawns.length) {
-						splashIdx = 0;
+			if (testSlick) {
+				MBus.publish(Constants.mbus_carousel_add, new Message.Add(carouselIdElt, '100%', '100%', splashLawns));
+				
+			}
+			else {
+				timer = setInterval(function () {
+					if (splashLawns.length > 1) {
+						Session.set('splashItem', splashLawns[splashIdx].url);
+						Session.set('splashId', splashLawns[splashIdx].id);
+						splashIdx++;
+						if (splashIdx == splashLawns.length) {
+							splashIdx = 0;
+						}
 					}
-				}
-			}, 5000);
+				}, 5000);
+			}
 		})
 	}
 });
@@ -63,5 +69,28 @@ Template.splash.helpers({
 	},
 	splashMode: function () {
 		return {topic: Constants.mbus_splashCarousel, html: carouselIdElt, type: 'splashItem', subType: null}
+	},
+	items: function () {
+		return [{pic: 'custom.png', email: 'junk@gmail.com', fullname: 'joe schmo'}];
+	}
+});
+
+Template.splash.animations({
+	".item": {
+		container: ".container-items", // container of the ".item" elements
+		in: "animated fast fadeIn", // class applied to inserted elements (animations courtesy of animate.css)
+		out: "animated fast fadeOut", // class applied to removed elements
+		inCallback: function () {
+			var title = $(this).find(".title").text();
+			console.log("Inserted " + title + " to the DOM");
+
+		},
+		outCallback: function () {
+			var title = $(this).find(".title").text();
+			console.log("Removed " + title + " from the DOM");
+		},
+		animateInitial: true, // animate the elements already rendered
+		animateInitialStep: 200, // Step between animations for each initial item
+		animateInitialDelay: 500 // Delay before the initial items animate
 	}
 });
